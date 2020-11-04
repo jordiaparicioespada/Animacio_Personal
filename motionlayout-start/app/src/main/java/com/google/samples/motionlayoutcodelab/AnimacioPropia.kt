f2dceb0c2f4d7b273871a588c8d7aa5b4898f241
jordiaparicioespada <jordiaparicioespada@gmail.com>
2020-11-04 19:01:03 +0100

    Prac

---
+++ motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/AnimacioPropia.kt
@@ -0,0 +1,14 @@
+package com.google.samples.motionlayoutcodelab
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class AnimacioPropia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step1)
+    }
+}
